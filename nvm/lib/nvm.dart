69ed3b381e430ed390546baa3ee707b63462e41f
HiImNVM <hackdaikk@gmail.com>
2018-12-20 05:34:00 +0700

    Update file analysis

---
+++ nvm/lib/nvm.dart
@@ -0,0 +1,13 @@
+library nvm;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+  void xxx() {
+    final String aB = 'a';
+    if (aB.isEmpty) {
+    }
+    return;
+  }
+}
