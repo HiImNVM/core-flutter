ace9f6d91aa2f48c3786ec0e3a76b4c7ffa09206
HiImNVM <hackdaikk@gmail.com>
2019-01-07 04:15:42 +0700

    Update core with new pub redux and refactor code

---
+++ nvm/lib/demo.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:nvm/nvm.dart';
+
+void main(List<String> args) {
+  NvmStore();
+}
--- nvm/lib/nvm.dart
+++ nvm/lib/nvm.dart
@@ -1,79 +1,22 @@
 library nvm;
 
-import 'dart:async';
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
-// redux
-typedef State NvmReducer<State>(State state);
-typedef void NvmMiddleware<State>(
-  NvmStore<State> store,
-  NvmNextDispatcher next,
-);
-typedef void NvmNextDispatcher();
-
-class NvmStore<State> {
-  final StreamController<State> _changeController;
-  State _state;
-  List<NvmNextDispatcher> _dispatchers;
-
-  State get state => this._state;
-  Stream<State> get onChange => this._changeController.stream;
+class NvmStore extends MaterialApp {
+  final Widget initialApp;
+  final Store<dynamic> store;
 
   NvmStore({
-    State initialState,
-    List<NvmMiddleware<State>> middleware = const <NvmMiddleware<State>>[],
-  }) : this._changeController = StreamController<State>.broadcast(sync: false) {
-    this._state = initialState;
-    this._dispatchers = _createDispatchers(
-      middleware,
-      _createReduceAndNotify(),
-    );
-  }
-
-  List<NvmNextDispatcher> _createDispatchers(
-    List<NvmMiddleware<State>> middleware,
-    NvmNextDispatcher reduceAndNotify,
-  ) {
-    final List<NvmNextDispatcher> dispatchers = <NvmNextDispatcher>[]
-      ..add(reduceAndNotify);
-
-    for (NvmMiddleware<State> nextMiddleware in middleware.reversed) {
-      final NvmNextDispatcher next = dispatchers.last;
-      dispatchers.add(
-        () => nextMiddleware(this, next),
-      );
-    }
-    return dispatchers.reversed.toList();
-  }
-
-  NvmNextDispatcher _createReduceAndNotify() =>
-      () => _changeController.add(_state);
-
-  void dispatch() => _dispatchers[0]();
-
-  Future<dynamic> teardown() {
-    _state = null;
-    return _changeController.close();
-  }
-}
-
-// core
-
-class System extends StatelessWidget {
-  final Widget initialApp;
-  NvmStore<dynamic> store;
-
-  System({
     Key key,
     this.initialApp,
     this.store,
   }) : super(key: key);
 
-  @override
   Widget build(BuildContext context) => StoreProvider<dynamic>(
-    store: 
-    child: this.initialApp,
-  );
+        store: this.store,
+        child: this.initialApp,
+      );
 }
