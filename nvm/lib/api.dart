44d036cd9c98f6a6afa21fb156b52033db72945f
HiImNVM <hackdaikk@gmail.com>
2019-01-26 04:31:17 +0700

    Update NvmAPI and NvmFutureBuilder

--- nvm/lib/api.dart
+++ nvm/lib/api.dart
@@ -5,18 +5,19 @@ import 'package:http/http.dart';
 enum eMethod { GET, POST }
 
 class NvmAPI {
+  static int timeOut = 5;
   static Future<dynamic> call(
-      eMethod method, String url, Map<String, dynamic> body,
+      eMethod method, String url, Map<dynamic, dynamic> body,
       [Map<String, String> headers]) async {
     Response response;
     headers = headers ?? {'Content-Type': 'application/json'};
     final String newUrl = '$url';
     if (eMethod.GET == method) {
-      response =
-          await get(newUrl, headers: headers).timeout(Duration(seconds: 5));
+      response = await get(newUrl, headers: headers)
+          .timeout(Duration(seconds: timeOut));
     } else if (eMethod.POST == method) {
       response = await post(newUrl, headers: headers, body: jsonEncode(body))
-          .timeout(Duration(seconds: 5));
+          .timeout(Duration(seconds: timeOut));
     } else {
       throw Exception('The method is not support');
     }
--- nvm/lib/widgets.dart
+++ nvm/lib/widgets.dart
@@ -6,10 +6,8 @@ typedef SuccessBuilder = Widget Function(BuildContext context, dynamic data);
 
 class NvmFutureBuilder<T> extends FutureBuilder<T> {
   @required
+  @override
   final Future<T> future;
-
-  @required
-  final AsyncWidgetBuilder<T> builder;
 
   @required
   final ErrorBuilder errorBuilder;
@@ -22,14 +20,24 @@ class NvmFutureBuilder<T> extends FutureBuilder<T> {
 
   NvmFutureBuilder(
       {Key key,
-      this.future,
-      this.builder,
-      this.errorBuilder,
-      this.loadingBuilder,
-      this.successBuilder})
-      : super(
-          key: key,
-          future: future,
-          builder: builder,
-        );
+      @required this.future,
+      @required this.errorBuilder,
+      @required this.loadingBuilder,
+      @required this.successBuilder})
+      : assert(future != null),
+        assert(loadingBuilder != null),
+        assert(errorBuilder != null),
+        assert(successBuilder != null),
+        super(
+            key: key,
+            future: future,
+            builder: (BuildContext context, AsyncSnapshot<T> snapShot) {
+              if (snapShot.hasError) {
+                return errorBuilder(context, snapShot.error.toString());
+              }
+
+              return snapShot.hasData
+                  ? successBuilder(context, snapShot.data)
+                  : loadingBuilder(context);
+            });
 }
