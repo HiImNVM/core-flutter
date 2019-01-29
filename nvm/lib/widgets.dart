80158f6063ede50e3803ef40be62f96d00c8454c
HiImNVM <hackdaikk@gmail.com>
2019-01-29 08:28:11 +0700

    Update new widgets

--- nvm/lib/widgets.dart
+++ nvm/lib/widgets.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 typedef ErrorBuilder = Widget Function(BuildContext context, String error);
@@ -5,17 +6,10 @@ typedef LoadingBuilder = Widget Function(BuildContext context);
 typedef SuccessBuilder = Widget Function(BuildContext context, dynamic data);
 
 class NvmFutureBuilder<T> extends FutureBuilder<T> {
-  @required
   @override
   final Future<T> future;
-
-  @required
   final ErrorBuilder errorBuilder;
-
-  @required
   final LoadingBuilder loadingBuilder;
-
-  @required
   final SuccessBuilder successBuilder;
 
   NvmFutureBuilder(
@@ -35,9 +29,46 @@ class NvmFutureBuilder<T> extends FutureBuilder<T> {
               if (snapShot.hasError) {
                 return errorBuilder(context, snapShot.error.toString());
               }
-
               return snapShot.hasData
                   ? successBuilder(context, snapShot.data)
                   : loadingBuilder(context);
+            });
+}
+
+typedef ResultWillPop = bool Function(bool isWillPop);
+
+class NvmWillPopScope extends WillPopScope {
+  @override
+  final Widget child;
+  final BuildContext context;
+  final ResultWillPop onWillPopReceive;
+
+  NvmWillPopScope({
+    Key key,
+    @required this.child,
+    @required this.onWillPopReceive,
+    @required this.context,
+  }) : super(
+            key: key,
+            child: child,
+            onWillPop: () async {
+              final bool result = await showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) => AlertDialog(
+                        title: Text('Bạn có muốn thoát khỏi ứng dụng?'),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('Không'),
+                            onPressed: () => Navigator.pop(context, false),
+                          ),
+                          FlatButton(
+                            child: Text('Có'),
+                            onPressed: () => Navigator.pop(context, true),
+                          ),
+                        ],
+                      ));
+
+              return onWillPopReceive(result);
             });
 }
