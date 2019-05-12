f4f75d935d442e5b4a44ad9e3c208538eb5f8582
NVM <hackdaikk@gmail.com>
2019-05-13 06:28:42 +0700

    Update params with object and move check body in method POST and PUT

--- nvm/lib/api.dart
+++ nvm/lib/api.dart
@@ -12,23 +12,20 @@ class NvmAPI {
   int timeOut = 5;
   String preURL = '';
 
-  Future<dynamic> call(
-      [MethodEnum method = MethodEnum.GET,
-      String url = '',
-      Map<dynamic, dynamic> body,
-      Map<String, String> headers = const {
-        HttpHeaders.contentTypeHeader: 'application/json'
-      }]) async {
+  Future<dynamic> call({
+    MethodEnum method = MethodEnum.GET,
+    String url = '',
+    Map<dynamic, dynamic> body,
+    Map<String, String> headers = const {
+      HttpHeaders.contentTypeHeader: 'application/json'
+    },
+  }) async {
     if (method == null) {
       throw Exception('The method is null or empty.');
     }
 
     if (url == null || url.isEmpty) {
       throw Exception('The URL is null or empty.');
-    }
-
-    if (body == null || body.isEmpty) {
-      throw Exception('The body is null or empty.');
     }
 
     if (headers == null || headers.isEmpty) {
@@ -42,9 +39,15 @@ class NvmAPI {
         response = await get(newUrl, headers: headers)
             .timeout(Duration(seconds: timeOut));
       } else if (MethodEnum.POST == method) {
+        if (body == null || body.isEmpty) {
+          throw Exception('The body is null or empty.');
+        }
         response = await post(newUrl, headers: headers, body: jsonEncode(body))
             .timeout(Duration(seconds: timeOut));
       } else if (MethodEnum.PUT == method) {
+        if (body == null || body.isEmpty) {
+          throw Exception('The body is null or empty.');
+        }
         response = await put(newUrl, headers: headers, body: jsonEncode(body))
             .timeout(Duration(seconds: timeOut));
       } else if (MethodEnum.DELETE == method) {
